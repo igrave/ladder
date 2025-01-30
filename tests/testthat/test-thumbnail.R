7cde44e5975787b9636584c6f61a259eeea357b8
igrave <isaac.gravestock@gmail.com>
2025-01-30 14:10:58 +0100

    try this

--- tests/testthat/test-thumbnail.R
+++ tests/testthat/test-thumbnail.R
@@ -1,6 +1,6 @@
 test_that("get_slide_img works", {
   skip_on_cran()
-  ladder_auth_internal("testing")
+  use_gauth_workflow(Sys.getenv("gauth_secret"))
   result <- get_slide_img("1Bu6ZD0mev_jYpLi-rbJnhl-X3xezY6i4cUPQBubJrEg", "p")
   expect_string(result)
   expect_file_exists(result)
