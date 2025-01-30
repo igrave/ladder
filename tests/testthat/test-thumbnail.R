1f31c5228d0d265c2b0541d06f6d4b12d3daaa10
igrave <isaac.gravestock@gmail.com>
2025-01-30 21:47:59 +0100

    don't skip

--- tests/testthat/test-thumbnail.R
+++ tests/testthat/test-thumbnail.R
@@ -1,5 +1,5 @@
 test_that("get_slide_img works", {
-  skip_on_cran()
+  # skip_on_cran()
   use_gauth_workflow(readLines("token.txt"))
   result <- get_slide_img("1Bu6ZD0mev_jYpLi-rbJnhl-X3xezY6i4cUPQBubJrEg", "p")
   expect_string(result)
