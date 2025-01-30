6856ed0a46548a762de3f50799d08bf4bb466402
igrave <isaac.gravestock@gmail.com>
2025-01-30 22:21:05 +0100

    try again

--- tests/testthat/test-thumbnail.R
+++ tests/testthat/test-thumbnail.R
@@ -1,8 +1,9 @@
 test_that("get_slide_img works", {
   # skip_on_cran()
-  expect_true(file.exists("token.txt"))
-  expect_true(file.size("token.txt") > 0)
-  accesstoken <- readLines("token.txt")[[1]]
+  path <- file.path(Sys.getenv("GITHUB_WORKSPACE"), "token.txt")
+  expect_true(file.exists(path))
+  expect_true(file.size(path) > 0)
+  accesstoken <- readLines(path)[[1]]
   expect_string(accesstoken)
   use_gauth_workflow(accesstoken)
   result <- get_slide_img("1Bu6ZD0mev_jYpLi-rbJnhl-X3xezY6i4cUPQBubJrEg", "p")
