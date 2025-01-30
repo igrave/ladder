0088970b6851fe2c2400be2042ec1c6a66fd9303
igrave <isaac.gravestock@gmail.com>
2025-01-30 23:10:56 +0100

    remove dummy tests

--- tests/testthat/test-thumbnail.R
+++ tests/testthat/test-thumbnail.R
@@ -1,10 +1,5 @@
-test_that("token exists", {
-  expect_string(Sys.getenv("access_token"), min.chars = 1)
-})
-
 test_that("get_slide_img works", {
   # skip_on_cran()
-  Sys.getenv("access_token")
   path <- file.path(Sys.getenv("HOME"), "token.txt")
   expect_true(file.exists(path))
   expect_true(file.size(path) > 0)
@@ -14,8 +9,4 @@ test_that("get_slide_img works", {
   result <- get_slide_img("1Bu6ZD0mev_jYpLi-rbJnhl-X3xezY6i4cUPQBubJrEg", "p")
   expect_string(result)
   expect_file_exists(result)
-})
-
-test_that("dummy", {
-  expect_true(TRUE)
 })
