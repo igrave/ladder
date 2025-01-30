e03ce0689ce14fca4f908ba089f17d62513e6b96
igrave <isaac.gravestock@gmail.com>
2025-01-30 21:44:31 +0100

    now use auth

--- tests/testthat/test-thumbnail.R
+++ tests/testthat/test-thumbnail.R
@@ -1,10 +1,10 @@
-# test_that("get_slide_img works", {
-#   skip_on_cran()
-#   use_gauth_workflow(readLines("token.txt"))
-#   result <- get_slide_img("1Bu6ZD0mev_jYpLi-rbJnhl-X3xezY6i4cUPQBubJrEg", "p")
-#   expect_string(result)
-#   expect_file_exists(result)
-# })
+test_that("get_slide_img works", {
+  skip_on_cran()
+  use_gauth_workflow(readLines("token.txt"))
+  result <- get_slide_img("1Bu6ZD0mev_jYpLi-rbJnhl-X3xezY6i4cUPQBubJrEg", "p")
+  expect_string(result)
+  expect_file_exists(result)
+})
 
 test_that("dummy", {
   expect_true(TRUE)
