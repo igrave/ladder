f14fd52fe60c0a29d3de62f8fe7c653a5e019a63
igrave <isaac.gravestock@gmail.com>
2025-05-01 16:23:52 +0200

    only align cells with content

--- R/flextable.R
+++ R/flextable.R
@@ -192,8 +192,9 @@ table_requests <- function(ft, table_id = table_id, part = c("header", "body", "
   par_style_requests <- paragraph_style(
     part_styles$pars,
     row_offset = row_offset,
+    has_text = part_spans$ind,
     objectId = table_id
   )
-  c(my_tab, par_style_requests)
+  my_tab <- c(my_tab, par_style_requests)
   my_tab
 }
